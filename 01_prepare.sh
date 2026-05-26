f364602634449fbb37cbcf0d7f12a05cc160933a
MiunaH <2749544180@qq.com>
2026-05-26 11:16:09 +0800

    @ ci: fix mt7621 compile error masking and build script robustness
    Remove continue-on-error from compile step to surface real build failures.
    Add fail-fast:false, timeout guards, and download retry logic to mt7621 workflow.
    Replace hardcoded line-number sed inserts with pattern-based matching in 01_prepare.sh.
    Add wget retries and mkdir -p for resilience against transient network failures.
    @

--- 01_prepare.sh
+++ 01_prepare.sh
@@ -6,7 +6,7 @@ sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
-sed -i '20i PKG_USE_MIPS16:=0' feeds/small/dns2tcp/Makefile
+sed -i '/^include \$(TOPDIR)\/rules.mk/a PKG_USE_MIPS16:=0' feeds/small/dns2tcp/Makefile
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
@@ -18,14 +18,14 @@ rm -rf feeds/luci/applications/luci-app-scutclient/
 git clone https://github.com/hanwckf/luci-app-scutclient.git feeds/luci/applications/luci-app-scutclient 
 
 #单独拉取联通加速
-mkdir package/scut-unicom
-wget https://raw.githubusercontent.com/wykdg/route_script/master/scut-unicom/Makefile -O package/scut-unicom/Makefile
+mkdir -p package/scut-unicom
+wget --tries=5 --timeout=30 https://raw.githubusercontent.com/wykdg/route_script/master/scut-unicom/Makefile -O package/scut-unicom/Makefile
 
 ./scripts/feeds install -a
 
 
 #在启动项加入联通加速
-sed -i '3i #如果使用联通加速，删除前面的#，并填充后面的值\n#sleep 10 && /usr/share/scut_unicom/add_route.sh server_ip username password' package/base-files/files/etc/rc.local 
+sed -i '/^exit 0/i #如果使用联通加速，删除前面的#，并填充后面的值\n#sleep 10 && /usr/share/scut_unicom/add_route.sh server_ip username password' package/base-files/files/etc/rc.local 
 #将ttyd改成默认root登录
 sed -i "s/option command '\/bin\/login'/option command '\/bin\/login -f root'/" feeds/packages/utils/ttyd/files/ttyd.config 
 
