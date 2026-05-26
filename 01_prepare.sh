5dbf7cd04976a7566011c3cd3606149e8e0d0b58
MiunaH <2749544180@qq.com>
2026-05-26 12:29:29 +0800

    @ fix: remove broken kenzo/small feeds and clone packages directly
    kenzo/small feeds have dropped openwrt-21.02 support, causing make defconfig
    to segfault. Remove both feeds, clone luci-theme-argon/scutclient/frp/scut-unicom
    from their source repos. Drop passwall/mosdns which no longer have 21.02-compatible
    sources.
    @

--- 01_prepare.sh
+++ 01_prepare.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
 set -e -o pipefail
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
-rm -rf feeds/packages/utils/v2dat
-sed -i '/^include \$(TOPDIR)\/rules.mk/a PKG_USE_MIPS16:=0' feeds/small/dns2tcp/Makefile
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+# kenzo/small feeds have dropped openwrt-21.02 support.
+# Use default immortalwrt feeds only, and directly clone needed extra packages.
 
-rm -rf feeds/packages/net/frp
-git clone https://github.com/kuoruan/openwrt-frp.git package/frp
+./scripts/feeds update -a
 
-#单独拉取scutclient
+# Clone luci-theme-argon from source (jerrykuku)
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+
+# Clone scutclient
 rm -rf feeds/luci/applications/luci-app-scutclient/
-git clone https://github.com/hanwckf/luci-app-scutclient.git feeds/luci/applications/luci-app-scutclient 
+git clone https://github.com/hanwckf/luci-app-scutclient.git feeds/luci/applications/luci-app-scutclient
 
-#单独拉取联通加速
+# Clone scut-unicom
 mkdir -p package/scut-unicom
 wget --tries=5 --timeout=30 https://raw.githubusercontent.com/wykdg/route_script/master/scut-unicom/Makefile -O package/scut-unicom/Makefile
 
+# Clone openwrt-frp
+rm -rf feeds/packages/net/frp
+git clone https://github.com/kuoruan/openwrt-frp.git package/frp
+
 ./scripts/feeds install -a
 
-
 #在启动项加入联通加速
-sed -i '/^exit 0/i #如果使用联通加速，删除前面的#，并填充后面的值\n#sleep 10 && /usr/share/scut_unicom/add_route.sh server_ip username password' package/base-files/files/etc/rc.local 
+sed -i '/^exit 0/i #如果使用联通加速，删除前面的#，并填充后面的值\n#sleep 10 && /usr/share/scut_unicom/add_route.sh server_ip username password' package/base-files/files/etc/rc.local
+
 #将ttyd改成默认root登录
-sed -i "s/option command '\/bin\/login'/option command '\/bin\/login -f root'/" feeds/packages/utils/ttyd/files/ttyd.config 
+sed -i "s/option command '\/bin\/login'/option command '\/bin\/login -f root'/" feeds/packages/utils/ttyd/files/ttyd.config
 
-
-sed  -i "s/exit 0/[ ! -f '\/usr\/sbin\/trojan' ] \&\& [ -f '\/usr\/bin\/trojan-go' ] \&\& ln -sf \/usr\/bin\/trojan-go \/usr\/bin\/trojan\nexit 0/" package/emortal/default-settings/files/99-default-settings                
+sed -i "s/exit 0/[ ! -f '\/usr\/sbin\/trojan' ] \&\& [ -f '\/usr\/bin\/trojan-go' ] \&\& ln -sf \/usr\/bin\/trojan-go \/usr\/bin\/trojan\nexit 0/" package/emortal/default-settings/files/99-default-settings
